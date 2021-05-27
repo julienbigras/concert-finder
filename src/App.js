c475f28693e7a0951b00486ab0b662cdc9341656
julienbigras <julien.bigras@hotmail.com>
2021-05-26 21:45:46 -0400

    working on implementing a dropdown form to choose the city for the ticketmaster api call

--- src/App.js
+++ src/App.js
@@ -5,15 +5,43 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    city: '',
+    // city: '',
+    selectValue: '',
     events: [],
+  };
+
+  handleCityChange = (e) => {
+    this.setState({ selectValue: e.target.value });
+    console.log(this.state.selectValue, 'select value');
+  };
+
+  updateCity = (e) => {
+    e.preventDefault();
+    console.log('hello i have been clicked');
+    // this.setState({ city: city })
   };
 
   render() {
     return (
       <div className="App">
-        <h1>hello</h1>
-        <Form />
+        <h1>Concert Wishlist</h1>
+        <form action="">
+          <label htmlFor="cities">Choose a city: </label>
+          <select
+            name="cities"
+            id="cities"
+            value={this.state.selectValue}
+            onChange={this.handleCityChange}
+          >
+            <option value="toronto">Toronto</option>
+            <option value="montreal">Montreal</option>
+            <option value="ottawa">Ottawa</option>
+            <option value="buffalo">Buffalo</option>
+            <option value="detroit">Detroit</option>
+          </select>
+          <button onClick={(e) => this.updateCity(e)}>click</button>
+        </form>
+        {/* <Form updateCity={this.updateCity} handleCityChange={this.handleCityChange} /> */}
         <EventList />
       </div>
     );
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class Form extends React.Component {
-  render() {
+  render(props) {
     return (
       <form action="">
-        <input type="text" />
-        <button>click</button>
+        <label htmlFor="cities">Choose a city: </label>
+        <select
+          name="cities"
+          id="cities"
+          value={this.state.selectValue}
+          onChange={this.props.handleChange}
+        >
+          <option value="toronto">Toronto</option>
+          <option value="montreal">Montreal</option>
+          <option value="ottawa">Ottawa</option>
+          <option value="buffalo">Buffalo</option>
+          <option value="detroit">Detroit</option>
+        </select>
+        <button onClick={(e) => this.props.updateCity(e)}>click</button>
       </form>
     );
   }
